cb83e0f63364be9091322062e56de19e090f7eeb
lucassouzah <lucassouzaluciano12@gmail.com>
2025-10-03 21:15:41 -0300

    conceitospython

---
+++ Variaveis.py
@@ -0,0 +1,9 @@
+# O input retorna os valores como string (int e float para especificar as informações )
+
+nome =  input ("digite seu nome:")
+idade = int (input ("Digite sua idade:"))
+peso = float(input("Digite o seu peso:"))
+
+print(nome)
+print(idade)
+print(peso)
